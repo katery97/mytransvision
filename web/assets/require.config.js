94ceec2489c5d62a50f704ead3e31c7be6fe102d
katery97 <62404171+katery97@users.noreply.github.com>
2021-01-20 20:20:39 +0800

    Add files via upload

---
+++ web/assets/require.config.js
@@ -0,0 +1,25 @@
+var components = {
+    "packages": [
+        {
+            "name": "jquery",
+            "main": "jquery-built.js"
+        },
+        {
+            "name": "jquery-autocomplete",
+            "main": "jquery-autocomplete-built.js"
+        },
+        {
+            "name": "clipboard.js",
+            "main": "clipboard.js-built.js"
+        }
+    ],
+    "baseUrl": "components"
+};
+if (typeof require !== "undefined" && require.config) {
+    require.config(components);
+} else {
+    var require = components;
+}
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = components;
+}
